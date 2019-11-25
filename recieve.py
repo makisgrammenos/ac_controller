b7976bdd20dcb9c38c8574fb8f8fa92030cb462a
Makisgrammenos <p19gram@ionio.gr>
2019-11-25 16:58:01 +0200

    Log functionality added

--- recieve.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-IP = "192.168.1.13"
-PORT = 5005
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind((IP,PORT))
-while True:
-    data,addr = sock.recvfrom(1024)
-    print data
